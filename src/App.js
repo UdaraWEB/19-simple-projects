2585c4c1d397a267c9c4a920896e7d1d001fa657
UdaraWEB <155995538+UdaraWEB@users.noreply.github.com>
2024-02-15 12:20:21 +0530

    simple project 3 - create the rating star

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 export default function App() {
   return (
     <div>
       <Accordian />
       <RandomColor />
+      <StarRating />
     </div>
   );
 }
