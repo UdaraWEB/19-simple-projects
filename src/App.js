214fbc3943627b545e4977b4b74a9668b0035441
UdaraWEB <155995538+UdaraWEB@users.noreply.github.com>
2024-02-15 11:14:20 +0530

    simple project 2 - create random color generate button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 export default function App() {
   return (
     <div>
       <Accordian />
+      <RandomColor />
     </div>
   );
 }
